bec59d2e51caab93ac7cdc689ebfc09744ad8f19
notromanramirez <66532078+notromanramirez@users.noreply.github.com>
2021-07-27 21:04:53 -0400

    Added input and output sections.

--- userInterface/degAnalysis_UI.R
+++ userInterface/degAnalysis_UI.R
@@ -1,7 +1,10 @@
 degAnalysisTab <- tabItem(tabName = "degAnalysis",
                                  fluidRow(
-                                   column(9,
-                                     HTML("DEG Analysis Page")
+                                   column(4,
+                                     ### PLACE INPUT CODE HERE ###
+                                   ),
+                                   column(5,
+                                     ### PLACE OUTPUT CODE HERE ###
                                    ),
                                    column(width = 3,
                                           div(style = "display:inline-block; float:right", 
