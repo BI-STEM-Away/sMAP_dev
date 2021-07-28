92d4cb4c94a6b1671215a142ff142d69fb676e27
notromanramirez <66532078+notromanramirez@users.noreply.github.com>
2021-07-27 21:18:58 -0400

    Removed main tab and added sub tabs.

--- userInterface/functionalAnalysis_UI.R
+++ userInterface/functionalAnalysis_UI.R
@@ -1,7 +1,47 @@
-functionalAnalysisTab <- tabItem(tabName = "functionalAnalysis",
-                      fluidRow(
-                        column(12,
-                          HTML("Functional Analysis Page")
-                        )
-                      )
-)
+# functionalAnalysisTab <- tabItem(tabName = "functionalAnalysis",
+#                       fluidRow(
+#                         column(12,
+#                           HTML("Functional Analysis Page")
+#                         )
+#                       )
+# )
+
+functionalEnrichmentAnalysisTab <- tabItem(tabName = "functionalEnrichmentAnalysis",
+                                           fluidRow(
+                                             column(6,
+                                                    HTML("Insert input FEA code here")
+                                                    ),
+                                             column(6,
+                                                    HTML("Insert output FEA code here")
+                                                    )
+                                             )
+                                           
+geneConceptNetworkTab <- tabItem(tabName = "geneConceptNetwork",
+                                           fluidRow(
+                                             column(6,
+                                                    HTML("Insert input GCN code here")
+                                                    ),
+                                             column(6,
+                                                    HTML("Insert output GCN code here")
+                                                    )
+                                             )
+                                 
+gseaTab <- tabItem(tabName = "gsea",
+                                           fluidRow(
+                                             column(6,
+                                                    HTML("Insert input GSEA code here")
+                                                    ),
+                                             column(6,
+                                                    HTML("Insert output GSEA code here")
+                                                    )
+                                             )
+                   
+transcriptionFactorAnalysisTab <- tabItem(tabName = "transcriptionFactorAnalysis",
+                                           fluidRow(
+                                             column(6,
+                                                    HTML("Insert input TFA code here")
+                                                    ),
+                                             column(6,
+                                                    HTML("Insert output TFA code here")
+                                                    )
+                                             )
