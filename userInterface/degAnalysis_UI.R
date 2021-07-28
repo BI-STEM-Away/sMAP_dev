c7ba3cbd837f682dc2b3cbcaac9813fd31bedaf0
notromanramirez <66532078+notromanramirez@users.noreply.github.com>
2021-07-27 21:11:55 -0400

    Removed main tab for sub tabs.

--- userInterface/degAnalysis_UI.R
+++ userInterface/degAnalysis_UI.R
@@ -1,13 +1,39 @@
-degAnalysisTab <- tabItem(tabName = "degAnalysis",
-                                 fluidRow(
-                                   column(4,
-                                     ### PLACE INPUT CODE HERE ###
+# degAnalysisTab <- tabItem(tabName = "degAnalysis",
+#                                  fluidRow(
+#                                    column(4,
+#                                      ### PLACE INPUT CODE HERE ###
+#                                    ),
+#                                    column(5,
+#                                      ### PLACE OUTPUT CODE HERE ###
+#                                    ),
+#                                    column(width = 3,
+#                                           div(style = "display:inline-block; float:right", 
+#                                               actionButton('degAnalyzed', label = 'Proceed', status = "success")))
+#                                  )
+# )
+
+volcanoPlotTab <- tabItem(tabName = "volcanoPlot",
+                          fluidRow(
+                            column(4,
+                                   ### PLACE INPUT VOLCANO PLOT CODE HERE ###
                                    ),
-                                   column(5,
-                                     ### PLACE OUTPUT CODE HERE ###
+                            column(5,
+                                   ### PLACE OUTPUT VOLCANO PLOT CODE HERE ###
                                    ),
-                                   column(width = 3,
+                            column(width = 3,
                                           div(style = "display:inline-block; float:right", 
                                               actionButton('degAnalyzed', label = 'Proceed', status = "success")))
                                  )
-)
+                          
+topDEGsTab <- tabItem(tabName = "topDEGs",
+                      fluidRow(
+                        column(4,
+                               ### PLACE INPUT DEGS CODE HERE ###
+                               ),
+                        column(5,
+                               ### PLACE OUTPUT DEGS CODE HERE ###
+                               ),
+                        column(width = 3,
+                                          div(style = "display:inline-block; float:right", 
+                                              actionButton('degAnalyzed', label = 'Proceed', status = "success")))
+                                 )
