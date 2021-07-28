e351551adbae94b7013d657f267972698e84bd14
nk468188 <82423925+nk468188@users.noreply.github.com>
2021-07-28 12:04:22 -0400

    Update sampleGrouping_UI.R

--- userInterface/sampleGrouping_UI.R
+++ userInterface/sampleGrouping_UI.R
@@ -1,11 +1,28 @@
 sampleGroupingTab <- tabItem(tabName = "sampleGrouping",
                            fluidRow(
                              column(4,
-                                    HTML("Insert Input Sample Grouping Code Here")
+                                    selectInput("grouping","Select how to group samples",choices=c("Metadata Feature","Manually")),
                              ),
                              column(5,
-                                    HTML("Insert Output Sample Grouping Code Here")
+                                    #Data Table to assign samples to groups manually for DEG analysis
+                                    conditionalPanel(condition="input.grouping=='Manually'",
+                                                     DT::DTOutput('grouptable'),
+                                                     actionButton("group1","Assign Selected Samples to Group 1"),
+                                                     actionButton("reset1","Reset Group 1"),
+                                                     actionButton("group2","Assign Selected Samples to Group 2"),
+                                                     actionButton("reset2","Reset Group 2"),
+                                                     actionButton("clear_rowsel","Clear Selected Rows"),
+                                                     p("Row Numbers in Group 1"),
+                                                     verbatimTextOutput("list_group1"),
+                                                     textOutput("group1_warn"),
+                                                     p("Row Numbers in Group 2"),
+                                                     verbatimTextOutput("list_group2"),
+                                                     textOutput("group2_warn")
+      
                              ),
+                                    #Assign groups based on metadata
+                                    conditionalPanel(condition="input.grouping=='Metadata Feature'",
+                                                     htmlOutput("col_selection"))
                              column(width = 3,
                                     div(style = "display:inline-block; float:right", 
                                         actionButton('grouped', label = 'Proceed', status = "success")))
