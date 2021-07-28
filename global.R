0ff3632d5c61063e19b61833b18a6e7a8d75976f
notromanramirez <66532078+notromanramirez@users.noreply.github.com>
2021-07-27 21:21:17 -0400

    Added more libraries.

--- global.R
+++ global.R
@@ -4,6 +4,19 @@ library(bs4Dash)
 library(shiny)
 library(fresh)
 
+# other imports
+packages1<-c("oligo","GEOquery","affy","limma","arrayQualityMetrics","sva","Biobase","affyPLM", "simpleaffy")#, "AffyBatch")
+for(x in packages1){
+    library(x,character.only=TRUE)
+}
+packages2<-c("ggplot2", "pheatmap")
+for(y in packages2){
+    library(y,character.only=TRUE)
+}
+packages12<-c("stringr","R.utils", "EnhancedVolcano", "shinyWidgets", "bs4Dash", "shiny", "fresh")
+for(y in packages12){
+    library(y,character.only=TRUE)
+
 #Import Functions from External R scripts.
 
 source("functions/testFunction.R", local = TRUE)$value
