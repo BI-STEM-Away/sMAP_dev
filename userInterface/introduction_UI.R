5f00dccd3ffca54499cfef45f8c5d43b583c535c
ivanlam27 <75362063+ivanlam27@users.noreply.github.com>
2021-07-29 23:19:51 +0100

    remove more button

--- userInterface/introduction_UI.R
+++ userInterface/introduction_UI.R
@@ -6,7 +6,7 @@ introductionTab <- tabItem(tabName = "introduction",
                                        align="center",
                                        #Introduction block 
                                        jumbotron(
-                                          status = "info",
+                                          status = "info",btnName = NULL,
                                           title = HTML("<b>STEM-Away Transcriptomics Pipeline Application</b>"),
                                           lead = "An application for processing quality control, statistical and functional analysis of a 
                                           GEO dataset in order to find potential biomarkers.",
