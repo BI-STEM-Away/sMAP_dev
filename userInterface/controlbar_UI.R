2f430af225e687ab2d010ec023ce20001794892b
ivanlam27 <75362063+ivanlam27@users.noreply.github.com>
2021-07-29 23:57:01 +0100

    change width dashboardcontrolbar

--- userInterface/controlbar_UI.R
+++ userInterface/controlbar_UI.R
@@ -1,6 +1,9 @@
 controlbar <- dashboardControlbar(
+  id = NULL,
+  disable = FALSE,
+  width = 500,
+  collapse = TRUE,
   skin = "light",
-  width = 100,
   controlbarMenu(
     h1("Questions? Feedback?"),
     p("Please leave us a comment in the link below"),
