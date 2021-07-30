adc3cbeef0981990e8dba6974ba19983a56f7b22
Disha Chauhan <67594262+disha-22@users.noreply.github.com>
2021-07-29 17:27:00 -0700

    Delete global.R

--- global.R
+++
@@ -1,9 +0,0 @@
-# Load the libraries and import function files here.
-# Global.R is run one time at app initiallization. 
-library(bs4Dash)
-library(shiny)
-library(fresh)
-
-#Import Functions from External R scripts.
-
-source("functions/testFunction.R", local = TRUE)$value
