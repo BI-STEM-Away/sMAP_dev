a73247a564e34dbdc54d79ac58112145a5bfc66d
nk468188 <82423925+nk468188@users.noreply.github.com>
2021-07-30 02:39:31 -0400

    Update sampleGrouping_UI.R

--- userInterface/sampleGrouping_UI.R
+++ userInterface/sampleGrouping_UI.R
@@ -20,30 +20,31 @@ sampleGroupingTab <- tabItem(tabName = "sampleGrouping",
                                       )
                                    
                                ),
-                                 column(6,
-                                        selectInput("grouping","Select how to group samples",choices=c("Metadata Feature","Manually")),
-                                 ),
+                                 # column(6,
+                                 #        selectInput("grouping","Select how to group samples",choices=c("Metadata Feature","Manually")),
+                                 # ),
                                  column(6,
                                         #Data Table to assign samples to groups manually for DEG analysis
-                                        conditionalPanel(condition="input.grouping=='Manually'",
-                                                         DT::DTOutput('grouptable'),
-                                                         actionButton("group1","Assign Selected Samples to Group 1"),
-                                                         actionButton("reset1","Reset Group 1"),
-                                                         actionButton("group2","Assign Selected Samples to Group 2"),
-                                                         actionButton("reset2","Reset Group 2"),
-                                                         actionButton("clear_rowsel","Clear Selected Rows"),
-                                                         p("Row Numbers in Group 1"),
-                                                         verbatimTextOutput("list_group1"),
-                                                         textOutput("group1_warn"),
-                                                         p("Row Numbers in Group 2"),
-                                                         verbatimTextOutput("list_group2"),
-                                                         textOutput("group2_warn")
-          
-                                 ),
+                                 #        conditionalPanel(condition="input.grouping=='Manually'",
+                                 #                         DT::DTOutput('grouptable'),
+                                 #                         actionButton("group1","Assign Selected Samples to Group 1"),
+                                 #                         actionButton("reset1","Reset Group 1"),
+                                 #                         actionButton("group2","Assign Selected Samples to Group 2"),
+                                 #                         actionButton("reset2","Reset Group 2"),
+                                 #                         actionButton("clear_rowsel","Clear Selected Rows"),
+                                 #                         p("Row Numbers in Group 1"),
+                                 #                         verbatimTextOutput("list_group1"),
+                                 #                         textOutput("group1_warn"),
+                                 #                         p("Row Numbers in Group 2"),
+                                 #                         verbatimTextOutput("list_group2"),
+                                 #                         textOutput("group2_warn")
+                                 # 
+                                 # ),
                                         #Assign groups based on metadata
-                                        conditionalPanel(condition="input.grouping=='Metadata Feature'",
+                                 #conditionalPanel(condition="input.grouping=='Metadata Feature'",
                                                          htmlOutput("col_selection")
-                                 ))
+                                # )
+                                 )
                            
                            )
 )
