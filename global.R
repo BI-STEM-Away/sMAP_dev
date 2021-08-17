6a69598b19d28c52bb5c094b71a11a00e5c872fd
nk468188 <82423925+nk468188@users.noreply.github.com>
2021-08-16 21:53:25 -0400

    Update global.R

--- global.R
+++ global.R
@@ -2,18 +2,36 @@
 # Global.R is run one time at app initiallization. 
 library(bs4Dash)
 library(shiny)
+library(shinybusy)
 library(fresh)
 
-# other imports
-packages1<-c("oligo","GEOquery","affy","limma","arrayQualityMetrics","sva","Biobase","affyPLM","EnhancedVolcano","AnnotationDbi","hgu133plus2.db","WGCNA","impute","org.Hs.eg.db","msigdbr","clusterProfiler","enrichplot","topGO" )#"simpleaffy")#), "AffyBatch")
-for(x in packages1){
-    library(x,character.only=TRUE)
-}
-packages2<-c("ggplot2", "pheatmap","shinyWidgets","DT","data.table","magrittr","tidyr","shinybusy")
-for(y in packages2){
-    library(y,character.only=TRUE)
-}
-packages12<-c("stringr","R.utils", "EnhancedVolcano", "shinyWidgets", "bs4Dash", "shiny", "fresh")
+library(oligo)
+library(GEOquery)
+library(affy)
+library(limma)
+library(arrayQualityMetrics)
+library(sva)
+library(Biobase)
+library(affyPLM)
+library(EnhancedVolcano)
+library(AnnotationDbi)
+library(hgu133plus2.db)
+library(WGCNA)
+library(clusterProfiler)
+library(msigdbr)
+library(impute)
+library(org.Hs.eg.db)
+library(ggplot2)
+library(pheatmap)
+library(shinyWidgets)
+library(DT)
+library(data.table)
+library(magrittr)
+library(tidyr)
+library(enrichplot)
+library(hugene11sttranscriptcluster.db)
+library(pd.huex.1.0.st.v2)
+packages12<-c("stringr","R.utils","shinyWidgets")
 for(y in packages12){
     library(y,character.only=TRUE)
 }
