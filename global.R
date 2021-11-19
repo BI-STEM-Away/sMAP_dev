25a0cbf85e722a7e2c4ab29b63581f1bd74d66ec
nk468188 <82423925+nk468188@users.noreply.github.com>
2021-11-19 15:23:48 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,7 +1,27 @@
-packages12<-c("bs4Dash","shiny","shinybusy","fresh","oligo","GEOquery","affy","limma","arrayQualityMetrics","sva","Biobase","affyPLM","EnhancedVolcano","AnnotationDbi","hgu133plus2.db","WGCNA","clusterProfiler","msigdbr","impute","org.Hs.eg.db","ggplot2","shinyWidgets","DT","data.table","enrichplot","hugene11sttranscriptcluster.db")
+packages12<-c("bs4Dash","shiny","shinybusy","fresh","oligo","GEOquery","affy","limma","arrayQualityMetrics","sva","Biobase","affyPLM","EnhancedVolcano","AnnotationDbi","hgu133plus2.db","WGCNA","clusterProfiler","msigdbr","impute","org.Hs.eg.db","ggplot2","shinyWidgets","DT","enrichplot","hugene11sttranscriptcluster.db")
 for(y in packages12){
+#    BiocManager::install(y)
     library(y,character.only=TRUE)
 }
-#Import Functions from External R scripts.
+library(knitr)
+library(Hmisc)
+library(htmltools)
+library(markdown)
+# library(tidyr)
+# library(magrittr)
+# library(stringr)
+# library(R.utils)
 
-source("functions/testFunction.R", local = TRUE)$value
+# Demo File paths
+demo.Samples.exp <- "www/demoData/Demo_Expression_Table.csv"
+demo.Samples.meta <- "www/demoData/Demo_Metadata.csv"
+demo.Samples.cel <- list("www/demoData/GSM494556.CEL.gz",
+                          "www/demoData/GSM494557.CEL.gz",
+                          "www/demoData/GSM494558.CEL.gz",
+                          "www/demoData/GSM494559.CEL.gz",
+                          
+                          "www/demoData/GSM494616.CEL.gz",
+                          "www/demoData/GSM494617.CEL.gz",
+                          "www/demoData/GSM494618.CEL.gz",
+                          "www/demoData/GSM494619.CEL.gz"
+                          )
